9ab0785d6b6e270ad669ef70e7651e99bf4b41d0
Blogapp <abul.hossain@gmail.com>
2023-07-27 23:03:02 +0600

    Pushing My Rails App

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+    layout 'application' # Inherit from application.html.erb
+  
+    def home
+      # Your controller action code here
+    end
+
+    def about
+        # Your controller action code here
+    end
+
+  end
